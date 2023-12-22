4b9c8e770f11948936e7939343b2bbd07991c4fd
muf <ahmadmuflih82@gmail.com>
2023-12-23 06:36:27 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>SukLib</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="/Photo/suklib.png" rel="icon">
+  <link href="/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <main>
+    <div class="container">
+    <div class="row">
+        <img src="/Photo/suklib.png" class="mx-auto" alt="" style="width: 180px; height: auto; padding-bottom: 20px; padding-top: 60px;">
+        <h3 style="text-align: center; color: #012970; padding-bottom : 20px">Selamat Datang di Sukapura Library!</h3>
+    </div>
+
+    <div class="row justify-content-center">
+      <div class="col-lg-3">
+          <div class="card">
+            <div class="card-body text-center">
+              <h5 class="card-title" style="text-align: center; font-size: 16px">Login Sebagai Member</h5>
+              <a class="nav-link collapsed" href="/member">
+                <i class="bi bi-person" style="font-size: 6rem; color:#F4683C"></i>
+              </a>
+            </div>
+          </div>
+      </div>
+
+      <div class="col-lg-3">
+          <div class="card">
+            <div class="card-body text-center">
+              <h5 class="card-title" style="text-align: center; font-size: 16px">Login Sebagai Admin</h5>
+              <a class="nav-link collapsed" href="/admin">
+                <i class="bi bi-person-workspace" style="font-size: 6rem; color:#012970"></i>
+              </a>
+            </div>
+          </div>
+      </div>
+    </div>
+
+    
+
+    </div>
+  </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/vendor/chart.js/chart.umd.js"></script>
+  <script src="/vendor/echarts/echarts.min.js"></script>
+  <script src="/vendor/quill/quill.min.js"></script>
+  <script src="/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="/vendor/tinymce/tinymce.min.js"></script>
+  <script src="/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/member', function () {
+    return view('loginmember');
+});
+
+Route::get('/admin', function () {
+    return view('loginadmin');
+});
+
+Route::get('/registadmin', function () {
+    return view('registadmin');
+});
+
+
+Route::get('/registmember', function () {
+    return view('registmember');
+});
+
+Route::get('/search', function () {
+    return view('search');
+});
+
+Route::get('/riwayat', function () {
+    return view('riwayat');
+});
+
+Route::get('/riwayatform', function () {
+    return view('riwayatform');
+});
+
+Route::get('/riwayatpengembalian', function () {
+    return view('riwayatpengembalian');
+});
